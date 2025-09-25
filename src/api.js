61602c9971a0e17e7c71e3cc2b8d785917259033
savickuse <savickuse@users.noreply.github.com>
2025-09-25 11:11:50 +0300

    build: update configuration

--- src/api.js
+++ src/api.js
@@ -104,3 +104,30 @@ class API {
 
 // Update 48
 module.exports = API;
+
+
+// API module for SporkUtils
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 55
+module.exports = API;
