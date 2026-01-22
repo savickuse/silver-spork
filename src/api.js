3df16930a2f9b254575f8afdbee97df387503d0e
savickuse <savickuse@users.noreply.github.com>
2026-01-22 20:13:12 +0200

    improve: update README

--- src/api.js
+++ src/api.js
@@ -77,3 +77,30 @@ class API {
 
 // Update 29
 module.exports = API;
+
+
+// API module for SporkUtils
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 48
+module.exports = API;
